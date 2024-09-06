1ec1a8480d551d199c64e2fdb85f31ae65845407
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2024-09-06 14:01:07 +0200

    Update client.lua

--- frameworks/ox/client.lua
+++ frameworks/ox/client.lua
@@ -2,6 +2,9 @@ if Link.framework ~= 'ox' and Link.framework ~= 'ox_core' then
     return
 end
 
+require '@ox_lib/init.lua'
+
+
 function NotifyViaFramework(message, type)
     lib.notify({ description = message, type = type, duration = 4000 })
 end
