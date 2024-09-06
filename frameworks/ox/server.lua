be9863e5c4286805d87d211e21d1cd46c60a97cb
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2024-09-06 14:02:06 +0200

    Update server.lua

--- frameworks/ox/server.lua
+++ frameworks/ox/server.lua
@@ -2,6 +2,9 @@ if Link.framework ~= 'ox' and Link.framework ~= 'ox_core' then
     return
 end
 
+require '@ox_lib/init.lua'
+local Ox = require '@ox_core/lib/init.lua'
+
 function CanPlayerAfford(player, amount)
     -- Not implemented by framework
     return true
