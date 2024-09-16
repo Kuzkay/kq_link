18ab9d35ae881f4abdf675bcb37799f2078e3227
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2024-09-16 09:38:01 +0200

    feat: Add codem-inventory support

---
+++ links/inventories/codem-inventory/server.lua
@@ -0,0 +1,24 @@
+if Link.inventory ~= 'codem-inventory' and Link.inventory ~= 'codem' then
+    return
+end
+
+function RegisterUsableItem(...)
+    return true -- Does not exist in this system
+end
+
+function GetPlayerItemData(player, item)
+    local data = exports['codem-inventory']:GetItemsByName(player, item)
+    return data
+end
+
+function GetPlayerItemCount(player, item)
+    return exports['codem-inventory']:GetItemsTotalAmount(player, item)
+end
+
+function AddPlayerItem(player, item, amount, meta)
+    return exports['codem-inventory']:AddItem(player, item, amount, nil, meta)
+end
+
+function RemovePlayerItem(player, item, amount)
+    return exports['codem-inventory']:RemoveItem(player, item, amount, nil)
+end
