f82866fe33f6fcf3d8fb3daec3bf1c40eaa68db6
Kuzkay <kuzkaygames@gmail.com>
2024-09-24 16:56:52 +0200

    fix: Added can carry item check for basic esx inv

--- links/frameworks/esx/server.lua
+++ links/frameworks/esx/server.lua
@@ -14,15 +14,15 @@ end
 
 function CanPlayerAfford(player, amount)
     local xPlayer = ESX.GetPlayerFromId(player)
-    
+
     if xPlayer.getAccount('money').money >= amount then
         return true
     end
-    
+
     if xPlayer.getAccount('bank').money >= amount then
         return true
     end
-    
+
     return false
 end
 
@@ -31,7 +31,7 @@ function AddPlayerMoney(player, amount, account)
     if not xPlayer then
         return false
     end
-    
+
     return xPlayer.addAccountMoney(account or 'money', amount)
 end
 
@@ -40,21 +40,21 @@ function RemovePlayerMoney(player, amount)
     if not xPlayer then
         return false
     end
-    
+
     if not CanPlayerAfford(player, amount) then
         return false
     end
-    
+
     if xPlayer.getAccount('money').money >= amount then
         xPlayer.removeAccountMoney('money', amount)
         return true
     end
-    
+
     if xPlayer.getAccount('bank').money >= amount then
         xPlayer.removeAccountMoney('bank', amount)
         return true
     end
-    
+
     return false
 end
 
@@ -62,24 +62,29 @@ if Link.inventory == 'framework' then
     function RegisterUsableItem(...)
         ESX.RegisterUsableItem(...)
     end
-    
+
     function GetPlayerItemData(player, item)
         local xPlayer = ESX.GetPlayerFromId(player)
-        
-        local data    = xPlayer.getInventoryItem(item)
-        return data
+
+        return xPlayer.getInventoryItem(item)
     end
-    
+
     function GetPlayerItemCount(player, item)
         local data = GetPlayerItemData(player, item)
         return data and (data.count or data.amount)
     end
-    
+
     function AddPlayerItem(player, item, amount, meta)
         local xPlayer = ESX.GetPlayerFromId(player)
-        return xPlayer.addInventoryItem(item, amount or 1, meta)
+
+        if xPlayer.canCarryItem(item, amount or 1) then
+            xPlayer.addInventoryItem(item, amount or 1, meta)
+            return true
+        else
+            return false
+        end
     end
-    
+
     function RemovePlayerItem(player, item, amount)
         local xPlayer = ESX.GetPlayerFromId(player)
         return xPlayer.removeInventoryItem(item, amount or 1)
@@ -88,6 +93,6 @@ end
 
 function GetPlayerCharacterId(player)
     local xPlayer = ESX.GetPlayerFromId(player)
-    
+
     return xPlayer.identifier
 end
