580a6bf6f2e650b78fdac30cca85aaad549b0385
Kuzkay <kuzkaygames@gmail.com>
2024-10-26 00:35:37 +0200

    chore: fxmanifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'KuzQuality.com | Kuzkay'
 description 'KuzQuality Link | Made to link existing frameworks and dependencies'
-version '1.1.3'
+version '1.1.4'
 
 provide 'kq_link'
 
