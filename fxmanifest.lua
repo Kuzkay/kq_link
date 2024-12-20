ce196ffc4fbc2079ee4ba8a1998ba65b9c2a5f93
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2024-12-20 11:29:55 +0100

    chore: update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'KuzQuality.com | Kuzkay'
 description 'KuzQuality Link | Made to link existing frameworks and dependencies'
-version '1.5.1'
+version '1.5.2'
 
 provide 'kq_link'
 
