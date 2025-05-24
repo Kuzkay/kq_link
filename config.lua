33763a2db8a16692640e71e6afb9f79a0d1752d5
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2025-05-24 14:20:35 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -42,8 +42,8 @@ Link = {
 --- 'core_inventory' -- Core inventory system (c8re)
 --- 'ak47_inventory' -- AK47 inventory system (menan)
 --- 'origen_inventory' -- Origen inventory system
--- 'chezza' -- Chezza inventory system (inventory)
-Link.inventory = 'framework'
+--- 'chezza' -- Chezza inventory system (inventory)
+Link.inventory = 'auto'
 
 
 --- NOTIFICATION OPTIONS
