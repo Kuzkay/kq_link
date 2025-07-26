27b76e883df82ff696e282b889aa8e4cae14d8ba
Kuzkay <kuzkaygames@gmail.com>
2025-07-26 16:02:16 +0200

    feat: Return item metadata on remove (ox_inv)

--- resource/server.lua
+++ resource/server.lua
@@ -1,15 +1,15 @@
 
 -- Custom useful functions
-function AddPlayerItemToFit(player, item, amount)
+function AddPlayerItemToFit(player, item, amount, meta)
     local gotItems = false
-    
+
     while not gotItems and amount > 0 do
-        gotItems = AddPlayerItem(player, item, amount)
-        
+        gotItems = AddPlayerItem(player, item, amount, meta)
+
         if not gotItems then
             amount = amount - 1
         end
     end
-    
+
     return gotItems, amount
 end
