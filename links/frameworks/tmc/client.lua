a1e4f679c9d90a6e9b4637c5ed5c3dcd899f87f0
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2025-11-25 14:08:39 +0100

    feat: Add client for TMC framework integration

---
+++ links/frameworks/tmc/client.lua
@@ -0,0 +1,27 @@
+if Link.framework ~= 'tmc' then
+    return
+end
+
+TMC = exports.core:getCoreObject()
+
+if TMC.Functions.GetPlayerData() and TMC.Functions.GetPlayerData().job then
+    PLAYER_DATA = TMC.Functions.GetPlayerData()
+end
+
+RegisterNetEvent('TMC:Client:OnPlayerLoaded')
+AddEventHandler('TMC:Client:OnPlayerLoaded', function()
+    PLAYER_DATA = TMC.Functions.GetPlayerData()
+end)
+
+RegisterNetEvent('TMC:Client:OnJobUpdate')
+AddEventHandler('TMC:Client:OnJobUpdate', function(jobData)
+    PLAYER_DATA.job = jobData
+end)
+
+function GetPlayerJob()
+    return PLAYER_DATA.job and PLAYER_DATA.job[1] and PLAYER_DATA.job[1].name
+end
+
+function NotifyViaFramework(message, type)
+    TMC.Functions.Notify(message, type, 4000)
+end
