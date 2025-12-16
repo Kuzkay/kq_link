28e62b361fd20133f3b1ef5e05a0e04195c9ef8d
Kuzkay <31634013+Kuzkay@users.noreply.github.com>
2025-12-16 16:01:55 +0100

    Bump version to 1.14.0 and add new server script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'KuzQuality.com | Kuzkay'
 description 'KuzQuality Link | Made to link existing frameworks and dependencies'
-version '1.13.1'
+version '1.14.0'
 
 server_scripts {
     -- RESOURCE BASE
@@ -28,6 +28,7 @@ server_scripts {
     'links/inventories/qs-inventory/server.lua',
     'links/inventories/origen_inventory/server.lua',
     'links/inventories/chezza/server.lua',
+    'links/inventories/jaksam/server.lua',
     'links/inventories/tgiann-inventory/server.lua',
     -- DISPATCH
     'links/dispatch/server/server.lua',
