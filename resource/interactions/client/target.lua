0d8a877d065b2c3171aa75e15bb5e18ecba2e114
Kuzkay <kuzkaygames@gmail.com>
2026-02-23 16:18:53 +0100

    fix: Fixed qb-target implementation
    Fixed issue when registering multiple interactions at the same coords for qb-target. As the ancient system does not understand it by default. Why not use ox_target anyway??

--- resource/interactions/client/target.lua
+++ resource/interactions/client/target.lua
@@ -1,10 +1,17 @@
 local SYSTEM = Link.input.target.system
 
+local _qbZoneCache = {}
+local _qbEntityCache = {}
+
+local function GetUniqueZoneKeyFromCoords(coords)
+    return string.format('%.2f_%.2f_%.2f', coords.x, coords.y, coords.z)
+end
+
 function InputUtils.AddEntityToTargeting(entity, message, event, canInteract, meta, maxDist, icon)
     if not Link.input.target.enabled or not SYSTEM then
         return
     end
-    
+
     if SYSTEM == 'interact' then
         return exports[SYSTEM]:AddLocalEntityInteraction({
             entity = entity,
@@ -23,7 +30,7 @@ function InputUtils.AddEntityToTargeting(entity, message, event, canInteract, me
             }
         })
     end
-    
+
     local options = {
         {
             type = 'client',
@@ -38,9 +45,33 @@ function InputUtils.AddEntityToTargeting(entity, message, event, canInteract, me
             end,
         }
     }
-    
+
     if SYSTEM == 'ox-target' or SYSTEM == 'ox_target' then
         return exports[SYSTEM]:addLocalEntity(entity, options)
+    elseif SYSTEM == 'qb-target' then
+        if not _qbEntityCache[entity] then
+            _qbEntityCache[entity] = { maxDist = maxDist, options = {} }
+        end
+
+        for _, o in ipairs(_qbEntityCache[entity].options) do
+            if o.event == event then return entity end
+        end
+
+        table.insert(_qbEntityCache[entity].options, options[1])
+        _qbEntityCache[entity].maxDist = math.max(_qbEntityCache[entity].maxDist, maxDist)
+
+        if _qbEntityCache[entity].registered then
+            exports[SYSTEM]:RemoveTargetEntity(entity)
+        end
+
+        exports[SYSTEM]:AddTargetEntity(entity, {
+            options = _qbEntityCache[entity].options,
+            distance = _qbEntityCache[entity].maxDist
+        })
+
+        _qbEntityCache[entity].registered = true
+
+        return entity
     else
         exports[SYSTEM]:AddTargetEntity(entity, {
             options = options,
@@ -54,9 +85,9 @@ function InputUtils.AddZoneToTargeting(coords, rotation, scale, message, event, 
     if not Link.input.target.enabled or not SYSTEM then
         return
     end
-    
+
     local identifier = math.random(0, 999999) .. '-' .. GetGameTimer()
-    
+
     if SYSTEM == 'interact' then
         return exports[SYSTEM]:AddInteraction({
             coords = coords,
@@ -72,9 +103,9 @@ function InputUtils.AddZoneToTargeting(coords, rotation, scale, message, event, 
             }
         })
     end
-    
+
     -- NON interact system
-    
+
     local options = {
         {
             type = 'client',
@@ -88,7 +119,7 @@ function InputUtils.AddZoneToTargeting(coords, rotation, scale, message, event, 
             end
         }
     }
-    
+
     if SYSTEM == 'ox-target' or SYSTEM == 'ox_target' then
         return exports[SYSTEM]:addBoxZone({
             coords = coords,
@@ -98,6 +129,47 @@ function InputUtils.AddZoneToTargeting(coords, rotation, scale, message, event, 
             drawSprite = true,
             options = options,
         })
+    elseif SYSTEM == 'qb-target' then
+        local key = GetUniqueZoneKeyFromCoords(coords)
+
+        if not _qbZoneCache[key] then
+            _qbZoneCache[key] = {
+                coords = coords,
+                scaleX = scale.x,
+                scaleY = scale.y,
+                zoneOpts = {
+                    name = key,
+                    debugPoly = Link.debugMode or false,
+                    useZ = true,
+                    minZ = coords.z - (scale.z / 2),
+                    maxZ = coords.z + (scale.z / 2),
+                },
+                maxDist = maxDist,
+                options = {},
+                registered = false,
+            }
+        end
+
+        for _, o in ipairs(_qbZoneCache[key].options) do
+            if o.event == event then return key end
+        end
+
+        table.insert(_qbZoneCache[key].options, options[1])
+        _qbZoneCache[key].maxDist = math.max(_qbZoneCache[key].maxDist, maxDist)
+
+        if _qbZoneCache[key].registered then
+            exports[SYSTEM]:RemoveZone(key)
+        end
+
+        local cache = _qbZoneCache[key]
+        exports[SYSTEM]:AddBoxZone(key, cache.coords, cache.scaleX, cache.scaleY, cache.zoneOpts, {
+            options = cache.options,
+            distance = cache.maxDist
+        })
+
+        _qbZoneCache[key].registered = true
+
+        return key
     else
         exports[SYSTEM]:AddBoxZone(identifier, coords, scale.x, scale.y, {
             name = identifier,
@@ -109,7 +181,7 @@ function InputUtils.AddZoneToTargeting(coords, rotation, scale, message, event, 
             options = options,
             distance = maxDist
         })
-        
+
         return identifier
     end
 end
@@ -118,7 +190,7 @@ function InputUtils.RemoveTargetZone(identifier)
     if not Link.input.target.enabled or not SYSTEM then
         return
     end
-    
+
     if SYSTEM == 'ox-target' or SYSTEM == 'ox_target' then
         exports[SYSTEM]:removeZone(identifier)
     elseif SYSTEM == 'interact' then
@@ -126,6 +198,36 @@ function InputUtils.RemoveTargetZone(identifier)
     else
         -- Solution for qb-target and qtarget
         exports[SYSTEM]:RemoveZone(identifier)
+        _qbZoneCache[identifier] = nil
+    end
+end
+
+function InputUtils.RemoveTargetZoneOption(coords, event)
+    if not Link.input.target.enabled or not SYSTEM or SYSTEM ~= 'qb-target' then
+        return
+    end
+
+    local key = GetUniqueZoneKeyFromCoords(coords)
+    local cache = _qbZoneCache[key]
+
+    if not cache then return end
+
+    for i, o in ipairs(cache.options) do
+        if o.event == event then
+            table.remove(cache.options, i)
+            break
+        end
+    end
+
+    exports[SYSTEM]:RemoveZone(key)
+
+    if #cache.options > 0 then
+        exports[SYSTEM]:AddBoxZone(key, cache.coords, cache.scaleX, cache.scaleY, cache.zoneOpts, {
+            options = cache.options,
+            distance = cache.maxDist
+        })
+    else
+        _qbZoneCache[key] = nil
     end
 end
 
@@ -133,7 +235,7 @@ function InputUtils.RemoveTargetEntity(identifier)
     if not Link.input.target.enabled or not SYSTEM then
         return
     end
-    
+
     if SYSTEM == 'ox-target' or SYSTEM == 'ox_target' then
         exports[SYSTEM]:removeLocalEntity(identifier)
     elseif SYSTEM == 'interact' then
@@ -141,6 +243,34 @@ function InputUtils.RemoveTargetEntity(identifier)
     else
         -- Solution for qb-target and qtarget
         exports[SYSTEM]:RemoveTargetEntity(identifier)
+        _qbEntityCache[identifier] = nil
     end
 end
 
+function InputUtils.RemoveTargetEntityOption(entity, event)
+    if not Link.input.target.enabled or not SYSTEM or SYSTEM ~= 'qb-target' then
+        return
+    end
+
+    local cache = _qbEntityCache[entity]
+
+    if not cache then return end
+
+    for i, o in ipairs(cache.options) do
+        if o.event == event then
+            table.remove(cache.options, i)
+            break
+        end
+    end
+
+    exports[SYSTEM]:RemoveTargetEntity(entity)
+
+    if #cache.options > 0 then
+        exports[SYSTEM]:AddTargetEntity(entity, {
+            options = cache.options,
+            distance = cache.maxDist
+        })
+    else
+        _qbEntityCache[entity] = nil
+    end
+end
