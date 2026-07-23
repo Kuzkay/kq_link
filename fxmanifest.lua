95d9ba1f6fe8b7e4fe8bb88c7a9ddd3633887e20
Kuzkay <kuzkaygames@gmail.com>
2026-07-23 19:05:56 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'KuzQuality.com | Kuzkay'
 description 'KuzQuality Link | Made to link existing frameworks and dependencies'
-version '1.26.0'
+version '1.27.0'
 
 server_scripts {
     -- RESOURCE BASE
